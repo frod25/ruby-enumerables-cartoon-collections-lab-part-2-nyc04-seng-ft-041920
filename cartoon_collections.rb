4050a6b778feafac9aa52c2a904564ab49241d8d
Felix Rodriguez <frod25@gmail.com>
2020-04-29 18:25:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  valid_calls.count do |call|
+  valid_calls.each do |call|
     planeteer_calls.find do |obj|
       obj == call
     end
